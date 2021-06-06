9a33a0a03f26bca9448e1576a546a502e3f35dae
Deathbaron <mullerdavid@deathbaron.org>
2021-06-06 22:17:22 +0200

    MultiActionbarKeyHide added

---
+++ MultiActionbarKeyHide/MultiActionbarKeyHide.lua
@@ -0,0 +1,31 @@
+local ADDON, T = ...
+local L = {}
+
+local function Init()
+	local frame = EnumerateFrames()
+	while frame 
+	do
+		if frame.IsProtected and frame:IsProtected() and frame.GetObjectType and frame:GetObjectType() == "CheckButton" and frame.action and frame.HotKey
+		then
+			local name = frame:GetName()
+			if string.find(name, "^MultiBarBottomLeftButton%d+$") or string.find(name, "^MultiBarBottomRightButton%d+$") or string.find(name, "^MultiBarLeftButton%d+$") or string.find(name, "^MultiBarRightButton%d+$")
+			then
+				frame.HotKey:Hide()
+				print(name)
+			end
+		end
+		frame = EnumerateFrames(frame)
+	end
+end
+
+local function OnEvent(self, event, arg1)
+	if event == "ADDON_LOADED" and arg1 == ADDON
+	then
+		self:UnregisterEvent("ADDON_LOADED")
+		Init()
+	end
+end
+
+local frame = CreateFrame("Frame")
+frame:RegisterEvent("ADDON_LOADED")
+frame:SetScript("OnEvent", OnEvent)
