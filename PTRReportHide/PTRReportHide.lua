e6ac6603bf1288e45592ddfbda1bfb3ce5449660
Deathbaron <user@deathbaron.org>
2022-04-25 19:58:05 +0200

    PTRReportHide tooltip removal added

--- PTRReportHide/PTRReportHide.lua
+++ PTRReportHide/PTRReportHide.lua
@@ -1,9 +1,26 @@
 local ADDON, T = ...
 local L = {}
 
+local function TooltipRemove()
+	for _,region in pairs({GameTooltip:GetRegions()}) 
+	do 
+		if region and region:GetObjectType() == "FontString" 
+		then 
+			local text = region:GetText() 
+			if text and strfind(text, "to submit a bug for this") 
+			then 
+				region:SetText(nil) 
+			end
+		end 
+	end 
+end
+
 local function Init()
 	PTR_IssueReporter:SetAlpha(0)
 	PTR_IssueReporter:SetScale(0.0001)
+	GameTooltip.ShowHook = TooltipRemove
+	GameTooltip:HookScript("OnShow", GameTooltip.ShowHook);
+	
 end
 
 local function OnEvent(self, event, arg1)
