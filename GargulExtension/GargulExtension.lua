8f4c6b00937d4a1ce99dd7325fe9e857bac5eb6e
Deathbaron <user@deathbaron.org>
2022-10-29 17:22:47 +0200

    Smaller bugfix and removed semicolons

--- GargulExtension/GargulExtension.lua
+++ GargulExtension/GargulExtension.lua
@@ -2,10 +2,11 @@ local ADDON, T = ...
 local L = {}
 
 local GL = nil
-local RollerUI = { Window = nil };
+local RollerUI = { Window = nil }
 
 local function TMBImport(self, data, triedToDecompress)
-	local firstLine = data:match("[^\n]+");
+	local firstLine = data:match("[^\n]+")
+
 	-- TMB Tooltip format
 	if (GL:strStartsWith(strtrim(firstLine), 'type,character_name,character_class')) then
 		
@@ -25,12 +26,14 @@ local function TMBImport(self, data, triedToDecompress)
 			if (not first) then
 				(function ()
 					
-					local CSVParts;
-					CSVParts = GL:strSplit(line, ",");
+					local CSVParts
+
+					CSVParts = GL:strSplit(line, ",")
 
 					-- We can't handle this line it seems
 					if (not CSVParts[2] or CSVParts[1] ~= "wishlist") then
-						return;
+						return
+
 					end
 					
 					local characterName = CSVParts[2]:lower()
@@ -47,7 +50,8 @@ local function TMBImport(self, data, triedToDecompress)
 					end
 					
 					if (not itemID) then
-						return;
+						return
+
 					end
 					
 					local key = received and "received" or "wishlists"
@@ -71,7 +75,8 @@ local function TMBImport(self, data, triedToDecompress)
 			first = false
 		end
 		
-		local jsonEncodeSucceeded, WebsiteData = pcall(function () return GL.JSON:encode(datatable); end);
+		local jsonEncodeSucceeded, WebsiteData = pcall(function () return GL.JSON:encode(datatable); end)
+
 		if jsonEncodeSucceeded
 		then
 			local ret = L.TMBImportOriginal(self, WebsiteData, true)
@@ -89,7 +94,7 @@ local function CountForItem(itemID)
 
     for bag = 0, 10 do
         for slot = 1, GetContainerNumSlots(bag) do
-            local _, itemCount, locked, _, _, _, _, _, _, bagItemID = GetContainerItemInfo(bag, slot);
+            local _, itemCount, locked, _, _, _, _, _, _, bagItemID = GetContainerItemInfo(bag, slot)
 
             if (bagItemID == itemID
                 and not locked -- The item is locked, aka it can not be put in the window
@@ -100,15 +105,16 @@ local function CountForItem(itemID)
         end
     end
 
-    return count;
+    return count
+
 end
 
 
 local function MasterLooterUIDraw(self, itemLink)
 	local ret = L.MasterLooterUIDrawOriginal(self, itemLink)
 	
-	local AceGUI = GL.AceGUI;
-	
+	local AceGUI = GL.AceGUI
+
     local ItemIcon = GL.Interface:getItem(self, "Icon.Item")
 	if ItemIcon
 	then
@@ -131,7 +137,8 @@ local function MasterLooterUIDraw(self, itemLink)
 				local stats = GL.DB.TMB.MetaData.stats and GL.DB.TMB.MetaData.stats[Entry.character:lower()]
 				stats = (stats and IsModifierKeyDown()) and string.format("(%d/%d)", stats["received"], stats["wishlists"]+stats["received"]) or ""
 				table.insert(WishListEntries, {sortingOrder, string.format("%s[%s]%s", playerName, prio, stats)})
-				itemIsOnSomeonesWishlist = true;
+				itemIsOnSomeonesWishlist = true
+
 			end
 
 			if itemIsOnSomeonesWishlist
@@ -139,8 +146,10 @@ local function MasterLooterUIDraw(self, itemLink)
 				local join = {}
 				local count = CountForItem(itemID)
 				table.sort(WishListEntries, function (a, b)
-					return a[1] < b[1];
-				end);
+					return a[1] < b[1]
+
+				end)
+
 				for _, Entry in pairs(WishListEntries) do
 					join[#join+1]=Entry[2]
 				end
@@ -160,14 +169,20 @@ end
 local function ReopenMasterLooterUIButtonDraw(self)
 	local ret = L.ReopenMasterLooterUIButtonDrawOriginal(self)
 	
-	local Button = GL.Interface:getItem(self, "Frame.OpenMasterLooterButton");
+	local Button = GL.Interface:getItem(self, "Frame.OpenMasterLooterButton")
+
 	if Button
 	then
-		local ButtonOverlay = Button:CreateTexture(nil, "OVERLAY");
-		ButtonOverlay:SetSize(16,16);
-		ButtonOverlay:SetPoint("TOPRIGHT", 5, 5);
-		ButtonOverlay:SetTexture("Interface\\GroupFrame\\UI-Group-MasterLooter");
-		Button.ButtonOverlay = ButtonOverlay;
+		local ButtonOverlay = Button:CreateTexture(nil, "OVERLAY")
+
+		ButtonOverlay:SetSize(16,16)
+
+		ButtonOverlay:SetPoint("TOPRIGHT", 5, 5)
+
+		ButtonOverlay:SetTexture("Interface\\GroupFrame\\UI-Group-MasterLooter")
+
+		Button.ButtonOverlay = ButtonOverlay
+
 	end
 	
 	
@@ -218,10 +233,11 @@ frame:SetScript("OnEvent", OnEvent)
 
 
 function RollerUI:show(time, itemLink, itemIcon, note, SupportedRolls)
-    GL:debug("RollerUI:show");
+    GL:debug("RollerUI:show")
 
     if (self.Window and self.Window:IsShown()) then
-        return false;
+        return false
+
     end
 
     -- Make sure we can adjust the roller UI accordingly when a player can't use the item
@@ -229,32 +245,42 @@ function RollerUI:show(time, itemLink, itemIcon, note, SupportedRolls)
         if (not userCanUseItem
             and GL.Settings:get("Rolling.dontShowOnUnusableItems", false)
         ) then
-            return false;
+            return false
+
         end
 
-        self:draw(time, itemLink, itemIcon, note, SupportedRolls, userCanUseItem);
-    end);
+        self:draw(time, itemLink, itemIcon, note, SupportedRolls, userCanUseItem)
 
-    return true;
+    end)
+
+    return true
+
 end
 
 function RollerUI:draw(time, itemLink, itemIcon, note, SupportedRolls, userCanUseItem)
-    GL:debug("RollerUI:draw");
+    GL:debug("RollerUI:draw")
 
 	local itemName = GL:getItemNameFromLink(itemLink)
 	
 	-- FrameXML\LootFrame.xml GroupLootFrameTemplate as base
 	
-    local Window = CreateFrame("Frame", "GargulUI_RollerUI_Window", UIParent, "BackdropTemplate");
-    Window:Hide();
-    Window:SetPoint(GL.Interface:getPosition("Roller"));
+    local Window = CreateFrame("Frame", "GargulUI_RollerUI_Window", UIParent, "BackdropTemplate")
 
-    Window:SetMovable(true);
-    Window:EnableMouse(true);
-    Window:SetClampedToScreen(true);
-    Window:SetFrameStrata("DIALOG");
+    Window:Hide()
+
+    Window:SetPoint(GL.Interface:getPosition("Roller"))
+
+    Window:SetMovable(true)
+
+    Window:EnableMouse(true)
+
+    Window:SetClampedToScreen(true)
+
+    Window:SetFrameStrata("DIALOG")
+
 	-- toplevel="true"
-    Window:SetSize(275, 84);
+    Window:SetSize(275, 84)
+
 	Window:SetBackdrop({
 		bgFile = "Interface\\DialogFrame\\UI-DialogBox-Gold-Background", 
 		edgeFile = "Interface\\DialogFrame\\UI-DialogBox-Gold-Border", 
@@ -262,93 +288,129 @@ function RollerUI:draw(time, itemLink, itemIcon, note, SupportedRolls, userCanUs
 		tileSize = 32, 
 		edgeSize = 32, 
 		insets = { left = 11, right = 12, top = 12, bottom = 11 } 
-	});
-    Window:RegisterForDrag("LeftButton");
-    Window:SetScript("OnDragStart", Window.StartMoving);
+	})
+
+    Window:RegisterForDrag("LeftButton")
+
+    Window:SetScript("OnDragStart", Window.StartMoving)
+
     Window:SetScript("OnDragStop", function()
-        Window:StopMovingOrSizing();
-        GL.Interface:storePosition(Window, "Roller");
-    end);
+        Window:StopMovingOrSizing()
+
+        GL.Interface:storePosition(Window, "Roller")
+
+    end)
+
     Window:SetScript("OnMouseDown", function (_, button)
         -- Close the roll window on right-click
         if (button == "RightButton") then
-            self:hide();
+            self:hide()
+
         end
-    end);
-    self.Window = Window;
-	
+    end)
+
+    self.Window = Window
+
 	Window.SlotTexture = Window:CreateTexture(nil, "ARTWORK")
-	Window.SlotTexture:SetSize(64, 64);
-	Window.SlotTexture:SetPoint("TOPLEFT", 3, -3);
-	Window.SlotTexture:SetTexture("Interface\\Buttons\\UI-EmptySlot");
-	
+	Window.SlotTexture:SetSize(64, 64)
+
+	Window.SlotTexture:SetPoint("TOPLEFT", 3, -3)
+
+	Window.SlotTexture:SetTexture("Interface\\Buttons\\UI-EmptySlot")
+
 	Window.IconFrame = CreateFrame("Button", nil, Window)
-    Window.IconFrame:EnableMouse(true);
-	Window.IconFrame:SetSize(34, 34);
-	Window.IconFrame:SetPoint("TOPLEFT", Window.SlotTexture, "TOPLEFT", 15, -15);
+    Window.IconFrame:EnableMouse(true)
+
+	Window.IconFrame:SetSize(34, 34)
+
+	Window.IconFrame:SetPoint("TOPLEFT", Window.SlotTexture, "TOPLEFT", 15, -15)
+
 	Window.IconFrame:SetScript("OnEnter", 
 		function(self)
-			GameTooltip:SetOwner(self, "ANCHOR_RIGHT");
-			GameTooltip:SetHyperlink(itemLink);
-			GameTooltip:Show();
-			CursorUpdate(self);
+			GameTooltip:SetOwner(self, "ANCHOR_RIGHT")
+
+			GameTooltip:SetHyperlink(itemLink)
+
+			GameTooltip:Show()
+
+			CursorUpdate(self)
+
 		end)
 	Window.IconFrame:SetScript("OnLeave", 
 		function(self)
-			GameTooltip:Hide();
-			ResetCursor();
+			GameTooltip:Hide()
+
+			ResetCursor()
+
 		end)
 	Window.IconFrame:SetScript("OnUpdate", 
 		function(self)
 			if ( GameTooltip:IsOwned(self) ) then
-				GameTooltip:SetOwner(self, "ANCHOR_RIGHT");
-				GameTooltip:SetHyperlink(itemLink);
+				GameTooltip:SetOwner(self, "ANCHOR_RIGHT")
+
+				GameTooltip:SetHyperlink(itemLink)
+
 			end
-			CursorOnUpdate(self);
+			CursorOnUpdate(self)
+
 		end)
 	Window.IconFrame:SetScript("OnClick", 
 		function(self)
-			HandleModifiedItemClick(itemLink);
+			HandleModifiedItemClick(itemLink)
+
 		end)
 	
 	Window.IconFrame.Icon = Window.IconFrame:CreateTexture(nil, "ARTWORK")
-	Window.IconFrame.Icon:SetSize(34, 34);
+	Window.IconFrame.Icon:SetSize(34, 34)
+
 	Window.IconFrame.Icon:SetPoint("TOPLEFT")
-	Window.IconFrame.Icon:SetTexture(itemIcon);
-	
+	Window.IconFrame.Icon:SetTexture(itemIcon)
+
 	Window.NameFrame = Window:CreateTexture(nil, "ARTWORK")
-	Window.NameFrame:SetSize(128, 64);
-	Window.NameFrame:SetPoint("LEFT", Window.SlotTexture, "RIGHT", -9, -10);
-	Window.NameFrame:SetTexture("Interface\\MerchantFrame\\UI-Merchant-LabelSlots");
-	
+	Window.NameFrame:SetSize(128, 64)
+
+	Window.NameFrame:SetPoint("LEFT", Window.SlotTexture, "RIGHT", -9, -10)
+
+	Window.NameFrame:SetTexture("Interface\\MerchantFrame\\UI-Merchant-LabelSlots")
+
 	Window.Name = Window:CreateFontString(nil, "ARTWORK", "GameFontNormalSmall")
-	Window.Name:SetText(itemName);
-	Window.Name:SetJustifyH("LEFT");
-	Window.Name:SetSize(90, 30);
-	Window.Name:SetPoint("LEFT", Window.SlotTexture, "RIGHT", -5, 5);
+	Window.Name:SetText(itemName)
+
+	Window.Name:SetJustifyH("LEFT")
+
+	Window.Name:SetSize(90, 30)
+
+	Window.Name:SetPoint("LEFT", Window.SlotTexture, "RIGHT", -5, 5)
+
 	GL:onItemLoadDo(itemLink, 
 		function(data) 
 			local quality = data[1].quality
-			local color = ITEM_QUALITY_COLORS[quality];
-			Window.Name:SetVertexColor(color.r, color.g, color.b);
+			local color = ITEM_QUALITY_COLORS[quality]
+
+			Window.Name:SetVertexColor(color.r, color.g, color.b)
+
 		end)
 	
 	Window.Decoration = Window:CreateTexture(nil, "OVERLAY")
-	Window.Decoration:SetSize(120, 120);
-	Window.Decoration:SetPoint("TOPLEFT", -30, 15);
-	Window.Decoration:SetTexture("Interface\\DialogFrame\\UI-DialogBox-Gold-Dragon");
-	
+	Window.Decoration:SetSize(120, 120)
+
+	Window.Decoration:SetPoint("TOPLEFT", -30, 15)
+
+	Window.Decoration:SetTexture("Interface\\DialogFrame\\UI-DialogBox-Gold-Dragon")
+
 	Window.Corner = Window:CreateTexture(nil, "OVERLAY")
-	Window.Corner:SetSize(32, 32);
-	Window.Corner:SetPoint("TOPRIGHT", -9, -7);
-	Window.Corner:SetTexture("Interface\\DialogFrame\\UI-DialogBox-Gold-Corner");
-	
-		
+	Window.Corner:SetSize(32, 32)
+
+	Window.Corner:SetPoint("TOPRIGHT", -9, -7)
+
+	Window.Corner:SetTexture("Interface\\DialogFrame\\UI-DialogBox-Gold-Corner")
+
 	-- if not userCanUseItem disable buttons
 	
-    local RollButtons = {};
-    local numberOfButtons = #SupportedRolls;
-	
+    local RollButtons = {}
+
+    local numberOfButtons = #SupportedRolls
+
 	if numberOfButtons > 3
 	then
 		local h = Window:GetHeight()
@@ -357,115 +419,154 @@ function RollerUI:draw(time, itemLink, itemIcon, note, SupportedRolls, userCanUs
 	end
 
     for i = 1, numberOfButtons do
-        local RollDetails = SupportedRolls[i] or {};
+        local RollDetails = SupportedRolls[i] or {}
 
-        local identifier = string.sub(RollDetails[1] or "", 1, 3);
-        local min = math.floor(tonumber(RollDetails[2]) or 0);
-        local max = math.floor(tonumber(RollDetails[3]) or 0);
+        local identifier = string.sub(RollDetails[1] or "", 1, 3)
+
+        local min = math.floor(tonumber(RollDetails[2]) or 0)
+
+        local max = math.floor(tonumber(RollDetails[3]) or 0)
 
         -- There are no more buttons to display
         if (GL:empty(identifier)) then
-            break;
+            break
+
         end
 
         -- Roll button
-        local Button = CreateFrame("Button", nil, Window, "GameMenuButtonTemplate");
-        Button:SetSize(66, 20);
-        Button:SetText(identifier);
-        Button:SetNormalFontObject("GameFontNormal");
-        Button:SetHighlightFontObject("GameFontNormal");
+        local Button = CreateFrame("Button", nil, Window, "GameMenuButtonTemplate")
+
+        Button:SetSize(66, 20)
+
+        Button:SetText(identifier)
+
+        Button:SetNormalFontObject("GameFontNormal")
+
+        Button:SetHighlightFontObject("GameFontNormal")
 
         if (not userCanUseItem) then
-            Button:Disable();
-            Button:SetMotionScriptsWhileDisabled(true);
+            Button:Disable()
+
+            Button:SetMotionScriptsWhileDisabled(true)
 
             -- Make sure rolling is still possible in case something was amiss!
             Button:SetScript("OnEnter", function()
-                Button:Enable();
-            end);
+                Button:Enable()
+
+            end)
 
             Button:SetScript("OnLeave", function()
-                Button:Disable();
-            end);
+                Button:Disable()
+
+            end)
+
         end
 
         Button:SetScript("OnClick", function ()
-            RandomRoll(min, max);
+            RandomRoll(min, max)
 
             if (GL.Settings:get("Rolling.closeAfterRoll")) then
-                self:hide();
-            else
-                local RollAcceptedNotification = GL.AceGUI:Create("InlineGroup");
-                RollAcceptedNotification:SetLayout("Fill");
-                RollAcceptedNotification:SetWidth(150);
-                RollAcceptedNotification:SetHeight(50);
-                RollAcceptedNotification.frame:SetParent(Window);
-                RollAcceptedNotification.frame:SetPoint("BOTTOMLEFT", Window, "TOPLEFT", 14, 4);
+                self:hide()
 
-                local Text = GL.AceGUI:Create("Label");
-                Text:SetText("Roll accepted!");
-                RollAcceptedNotification:AddChild(Text);
-                Text:SetJustifyH("MIDDLE");
+            else
+                local RollAcceptedNotification = GL.AceGUI:Create("InlineGroup")
+
+                RollAcceptedNotification:SetLayout("Fill")
+
+                RollAcceptedNotification:SetWidth(150)
+
+                RollAcceptedNotification:SetHeight(50)
+
+                RollAcceptedNotification.frame:SetParent(Window)
+
+                RollAcceptedNotification.frame:SetPoint("BOTTOMLEFT", Window, "TOPLEFT", 14, 4)
+
+                local Text = GL.AceGUI:Create("Label")
+
+                Text:SetText("Roll accepted!")
+
+                RollAcceptedNotification:AddChild(Text)
+
+                Text:SetJustifyH("MIDDLE")
 
                 self.RollAcceptedTimer = GL.Ace:ScheduleTimer(function ()
-                    RollAcceptedNotification.frame:Hide();
-                end, 2);
+                    RollAcceptedNotification.frame:Hide()
+
+                end, 2)
+
             end
-        end);
+        end)
 
         if (i == 1) then
-            Button:SetPoint("TOPRIGHT", Window, "TOPRIGHT", -36, -10);
+            Button:SetPoint("TOPRIGHT", Window, "TOPRIGHT", -36, -10)
+
         else
-            Button:SetPoint("TOPRIGHT", RollButtons[i - 1], "BOTTOMRIGHT", 0, -1);
+            Button:SetPoint("TOPRIGHT", RollButtons[i - 1], "BOTTOMRIGHT", 0, -1)
+
         end
 
-        tinsert(RollButtons, Button);
+        tinsert(RollButtons, Button)
+
     end
 	
 	Window.RollButtons = RollButtons
 
-    Window.PassButton = CreateFrame("Button", "GargulUI_RollerUI_Pass", Window, "UIPanelCloseButton");
-    Window.PassButton:SetPoint("TOPRIGHT", Window, "TOPRIGHT", -5, -3);
+    Window.PassButton = CreateFrame("Button", "GargulUI_RollerUI_Pass", Window, "UIPanelCloseButton")
+
+    Window.PassButton:SetPoint("TOPRIGHT", Window, "TOPRIGHT", -5, -3)
+
     Window.PassButton:SetScript("OnClick", function ()
-        self:hide();
-    end);
+        self:hide()
 
-    self:drawCountdownBar(time, itemLink, itemIcon, note, userCanUseItem);
+    end)
 
-    Window:Show();
+    self:drawCountdownBar(time, itemLink, itemIcon, note, userCanUseItem)
+
+    Window:Show()
+
 end
 
 function RollerUI:drawCountdownBar(time, itemLink, itemIcon, note, userCanUseItem)
-    GL:debug("RollerUI:drawCountdownBar");
+    GL:debug("RollerUI:drawCountdownBar")
 
     -- This shouldn't be possible but you never know!
     if (not self.Window) then
-        return false;
+        return false
+
     end
 	
 	local Window = self.Window
 	
 	Window.Timer = CreateFrame("StatusBar", nil, Window)
-	Window.Timer:SetSize(152, 10);
-	Window.Timer:SetPoint("TOPLEFT", Window.SlotTexture, "BOTTOMLEFT", 13, 10);
-	Window.Timer:SetMinMaxValues(0, 60000);
-	Window.Timer:SetValue(0);
+	Window.Timer:SetSize(152, 10)
+
+	Window.Timer:SetPoint("TOPLEFT", Window.SlotTexture, "BOTTOMLEFT", 13, 10)
+
+	Window.Timer:SetMinMaxValues(0, 60000)
+
+	Window.Timer:SetValue(0)
+
 	Window.Timer:SetStatusBarTexture("Interface\\PaperDollInfoFrame\\UI-Character-Skills-Bar")
-	Window.Timer:SetStatusBarColor(1, 1, 0);
+	Window.Timer:SetStatusBarColor(1, 1, 0)
+
 	Window.Timer.Bar = Window.Timer:GetStatusBarTexture()
+	Window.Timer.Bar:SetDrawLayer("ARTWORK")
 	
 	Window.Timer.Background = Window.Timer:CreateTexture(nil, "BACKGROUND")
 	Window.Timer.Background:SetAllPoints(Window.Timer)
 	Window.Timer.Background:SetColorTexture(0, 0, 0)
 	
-	Window.Timer.Border = Window.Timer:CreateTexture(nil, "ARTWORK")
-	Window.Timer.Border:SetSize(156, 20);
-	Window.Timer.Border:SetPoint("TOP", 0, 5);
-	Window.Timer.Border:SetTexture("Interface\\PaperDollInfoFrame\\UI-Character-Skills-BarBorder");
-	
+	Window.Timer.Border = Window.Timer:CreateTexture(nil, "BORDER")
+	Window.Timer.Border:SetSize(156, 20)
+
+	Window.Timer.Border:SetPoint("TOP", 0, 5)
+
+	Window.Timer.Border:SetTexture("Interface\\PaperDollInfoFrame\\UI-Character-Skills-BarBorder")
+
 	Window.Timer.start = GetTime()
-	Window.Timer:SetMinMaxValues(0, time);
-	Window.Timer:GetStatusBarTexture():SetDrawLayer("BACKGROUND");
+	Window.Timer:SetMinMaxValues(0, time)
+
+
 	Window.Timer:SetScript("OnUpdate", 
 		function(self)
 			local left = self.start and (GetTime()-self.start) or 0
@@ -478,12 +579,15 @@ function RollerUI:drawCountdownBar(time, itemLink, itemIcon, note, userCanUseIte
 end
 
 function RollerUI:hide()
-    GL:debug("RollerUI:hide");
+    GL:debug("RollerUI:hide")
 
     if (not self.Window) then
-        return;
+        return
+
     end
 
-    self.Window:Hide();
-    self.Window = nil;
+    self.Window:Hide()
+
+    self.Window = nil
+
 end
